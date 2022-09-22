2f16cb15b906777f71dae322b13d2eeaed5eab7f
Eduard Cuba <eduard.cuba@cern.ch>
2022-09-22 11:49:31 +0200

    Add agglomerative clustering

--- reco/plotting.py
+++ reco/plotting.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-import networkx as nx
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
 
 from .event import remap_arrays_by_label
 
@@ -160,3 +161,26 @@ def plot_remapped(tracksters, eid, labels):
     ax = fig.add_subplot(111, projection='3d')
     plot_tracksters(ax, rx, ry, rz, re)
     plt.show()
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
